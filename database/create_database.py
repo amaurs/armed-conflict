ef9f3b34684251437bf97ea2cf97113c7b70bde0
amaurs <amaury.gtz@gmail.com>
2016-05-12 05:41:25 -0500

    Final schema and class to populate.

--- database/create_database.py
+++ database/create_database.py
@@ -11,22 +11,22 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sshtunnel import SSHTunnelForwarder
-import csv
  
 BASE = declarative_base()
 
 class Country(BASE):
     __tablename__ = 'country'
     country_id = Column(Integer, primary_key=True)
-    name = Column(String())
+    name = Column(String(), unique=True)
     gwno = Column(Integer())
 class Actor(BASE):
     __tablename__ = 'actor'
     actor_id = Column(Integer, primary_key=True)
-    actor_type = Column(String(100))
+    actor_type = Column(String(500))
 class Region(BASE):
     __tablename__ = 'region'
     region_id = Column(Integer, primary_key=True)
+    name = Column(String(), unique=True)
     country_id = Column(Integer, ForeignKey('country.country_id'))
     country = relationship('Country') 
 class Interaction(BASE):
@@ -36,11 +36,28 @@ class Interaction(BASE):
 class Event(BASE):
     __tablename__ = 'event'
     event_id = Column(Integer, primary_key=True)
-    event_id_cnty = Column(String(4))
+    event_id_cnty = Column(String())
     event_id_no_cnty = Column(Integer())
     event_date = Column(Date())
     time_precision = Column(Integer())
     event_type = Column(String(100))
+    actor_1_id = Column(Integer, ForeignKey('actor.actor_id'))
+    actor_ally_1_id = Column(Integer, ForeignKey('actor.actor_id'))
+    actor_2_id = Column(Integer, ForeignKey('actor.actor_id'))
+    actor_ally_2_id = Column(Integer, ForeignKey('actor.actor_id'))
+    
+    actor_1 = relationship('Actor', foreign_keys=actor_1_id)
+    actor_ally_1 = relationship('Actor', foreign_keys=actor_ally_1_id)
+    actor_2 = relationship('Actor', foreign_keys=actor_2_id)
+    actor_ally_2 = relationship('Actor', foreign_keys=actor_ally_2_id)
+    
+    
+    inter_1_id = Column(Integer, ForeignKey('interaction.interaction_id'))
+    inter_2_id = Column(Integer, ForeignKey('interaction.interaction_id'))
+    inter_1 = relationship('Interaction', foreign_keys=inter_1_id)
+    inter_2 = relationship('Interaction', foreign_keys=inter_2_id)
+    
+    interaction = Column(Integer)
     country_id = Column(Integer, ForeignKey('country.country_id'))
     country = relationship('Country')
     admin_1_id = Column(Integer, ForeignKey('region.region_id'))
@@ -58,6 +75,7 @@ class Event(BASE):
     notes = Column(String(500))
     fatalities = Column(Integer())
 
+'''
 class Conflict(BASE):
     __tablename__ = 'conflict'
     interaction_id = Column(Integer, primary_key=True)
@@ -67,21 +85,21 @@ class Conflict(BASE):
     actor_2_id = Column(Integer, ForeignKey('actor.actor_id'))
     actor_ally_2_id = Column(Integer, ForeignKey('actor.actor_id'))
     
-    actor_1 = relationship('Actor', foreign_keys='actor_1_id')
-    actor_ally_1 = relationship('Actor', foreign_keys='actor_ally_1_id')
-    actor_2 = relationship('Actor', foreign_keys='actor_2_id')
-    actor_ally_2 = relationship('Actor', foreign_keys='actor_ally_2_id')
+    actor_1 = relationship('Actor', foreign_keys=actor_1_id)
+    actor_ally_1 = relationship('Actor', foreign_keys=actor_ally_1_id)
+    actor_2 = relationship('Actor', foreign_keys=actor_2_id)
+    actor_ally_2 = relationship('Actor', foreign_keys=actor_ally_2_id)
     
     
     inter_1_id = Column(Integer, ForeignKey('interaction.interaction_id'))
     inter_2_id = Column(Integer, ForeignKey('interaction.interaction_id'))
-    inter_1 = relationship('Interaction', foreign_keys='inter_1_id')
-    inter_2 = relationship('Interaction', foreign_keys='inter_2_id')
+    inter_1 = relationship('Interaction', foreign_keys=inter_1_id)
+    inter_2 = relationship('Interaction', foreign_keys=inter_2_id)
     
     interaction = Column(Integer)
-    event = Column(Integer, ForeignKey('event.event_id'))
-    actor = relationship('Event')
-    
+    event_id = Column(Integer, ForeignKey('event.event_id'))
+    event = relationship('Event')
+'''
 
 if __name__ == '__main__':
     with SSHTunnelForwarder(
